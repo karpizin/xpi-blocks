bf68956bd9695fc1c29760608092470b70a6e711
Slava Karpizin <viacheslav.karpizin@gmail.com>
2026-01-23 03:33:38 +0300

    docs: translate hexapod demo scripts to english

--- projects/hexapod_master/scripts/demos/body_dance.sh
+++ projects/hexapod_master/scripts/demos/body_dance.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 echo "Hexapod Body Dance initiated..."
 
-# Наклон вперед-влево
+# Tilt forward-left
 ros2 topic pub --once /hexapod/body_pose geometry_msgs/Pose "{orientation: {x: 0.1, y: 0.1, z: 0.0, w: 0.99}}"
 sleep 1
 
-# Наклон вперед-вправо
+# Tilt forward-right
 ros2 topic pub --once /hexapod/body_pose geometry_msgs/Pose "{orientation: {x: -0.1, y: 0.1, z: 0.0, w: 0.99}}"
 sleep 1
 
-# Наклон назад-вправо
+# Tilt backward-right
 ros2 topic pub --once /hexapod/body_pose geometry_msgs/Pose "{orientation: {x: -0.1, y: -0.1, z: 0.0, w: 0.99}}"
 sleep 1
 
-# Наклон назад-влево
+# Tilt backward-left
 ros2 topic pub --once /hexapod/body_pose geometry_msgs/Pose "{orientation: {x: 0.1, y: -0.1, z: 0.0, w: 0.99}}"
 sleep 1
 
-# Возврат
+# Return
 ros2 topic pub --once /hexapod/body_pose geometry_msgs/Pose "{orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}"
 echo "Dance finished."
